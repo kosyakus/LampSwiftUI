786405b5d81944b3d50d51e0d4cfc49a9192aeab
Natali <ns@numbuster.com>
2024-10-24 11:58:57 +0300

    Initial Commit

---
+++ LampSwiftUI/LampSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampSwiftUIApp.swift
+//  LampSwiftUI
+//
+//  Created by Natalia Sinitsyna on 24.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LampSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
